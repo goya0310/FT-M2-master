43efde468b3cd9072dbb629d18e633f3dbb310ac
Diego Llaya <goya0310@gmail.com>
2022-09-15 09:57:01 -0300

    homework 03

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,14 +1,27 @@
 var traverseDomAndCollectElements = function(matchFunc, startEl) {
   var resultSet = [];
-
+  // var $ = function(selector) {
+  //   var elements;
+  //   var selectorMatchFunc = matchFunctionMaker(selector);
+  //   elements = traverseDomAndCollectElements(selectorMatchFunc);
+  //   return elements;
+  // };
   if (typeof startEl === "undefined") {
     startEl = document.body;
   }
-
+  
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
-
+  
   // TU CÓDIGO AQUÍ
+  if(matchFunc(startEl)) resultSet.push(startEl);
+  for (let i = 0; i<startEl.children.length; i++){
+    const child = startEl.children[i];
+    let found = traverseDomAndCollectElements(matchFunc, child); // va guardando los resultSet en cada recursion
+    resultSet = [...resultSet, ...found] // concateno los rdos
+  }
+
+  return resultSet;
   
 };
 
@@ -18,7 +31,10 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === '#') return 'id';
+  if(selector[0] === '.') return 'class';
+  if(selector.split('.').length > 1) return 'tag.class';
+  else return 'tag'
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -27,17 +43,25 @@ var selectorTypeMatcher = function(selector) {
 // matchea el selector.
 
 var matchFunctionMaker = function(selector) {
+  
   var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
+  var matchFunction = function (sample){
   if (selectorType === "id") { 
-   
+    return sample.id === selector.substring(1);  
   } else if (selectorType === "class") {
-    
+    if(sample.classList.contains(`${selector.substring(1)}`)) return true;
+    else return sample.className === selector.substring(1);
   } else if (selectorType === "tag.class") {
+    if(sample.tagName && (sample.tagName.toLowerCase() === selector.split('.')[0].toLowerCase())){
+      if(sample.classList.contains(`${selector.split('.')[1].toLowerCase()}`)) return true;
+      else return sample.className === selector.split('.')[1].toLowerCase();
+    }
+    else return false;
     
   } else if (selectorType === "tag") {
-    
+    return sample.tagName && (sample.tagName.toLowerCase() === selector.toLowerCase());
   }
+  };
   return matchFunction;
 };
 
