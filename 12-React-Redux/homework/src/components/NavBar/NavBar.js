70e94d977b2cd3707af940537bed86bf158d8507
Diego Llaya <goya0310@gmail.com>
2022-10-14 12:20:02 -0300

    tarea React-Redux con Hook creado

--- 12-React-Redux/homework/src/components/NavBar/NavBar.js
+++ 12-React-Redux/homework/src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { useCountMoviesFavorites } from "../../hooks/useCountMoviesFavorites" 
 import Logo from '../../logoHenry.png'
 
 import './Navbar.css';
 
 export default function NavBar() {
+    const {countFav} = useCountMoviesFavorites();
+    
     return (
         <header className="navbar">
             <div>
@@ -14,7 +17,7 @@ export default function NavBar() {
                 <ul className="list">
                     <li className="list-item">
                         <NavLink exact to="/" >Home</NavLink>
-                        <NavLink to="/favs" >Favoritas</NavLink>
+                        <NavLink to="/favs" >{`Favoritas (${countFav})`}</NavLink>
                     </li>
                 </ul>
             </nav>
